3ee0468f1ffb5dca22731144433c1cd799c757d3
Jana Essmat <janaessmat@gmail.com>
2026-08-29 19:28:03 +0300

    added the patient management part where patients can view the upcoming and past appointments

--- src/main/java/com/meditrack/meditrack_backend/repository/AppointmentRepository.java
+++ src/main/java/com/meditrack/meditrack_backend/repository/AppointmentRepository.java
@@ -3,12 +3,12 @@ package com.meditrack.meditrack_backend.repository;
 import com.meditrack.meditrack_backend.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public interface AppointmentRepository
-        extends JpaRepository<Appointment, Long> {
 
-    List<Appointment> findByPatientId(Long patientId);
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findUpcomingByPatientId(Long patiendId, LocalDate now);
 
-    List<Appointment> findByDoctorId(Long doctorId);
+    List<Appointment> findPastByPatientId(Long patientId, LocalDate now);
 }
