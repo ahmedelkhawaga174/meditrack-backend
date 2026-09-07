b1404c05976a61b34bff9c84fb68cb78c3e6dad8
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-09-07 18:28:08 +0300

    add annotation on class in patient reponse

--- src/main/java/com/meditrack/meditrack_backend/dto/PatientResponse.java
+++ src/main/java/com/meditrack/meditrack_backend/dto/PatientResponse.java
@@ -1,8 +1,14 @@
 package com.meditrack.meditrack_backend.dto;
 import com.meditrack.meditrack_backend.enums.AppointmentStatus;
+import lombok.*;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class PatientResponse {
     private Long appointmentId;
     private String doctorName;
@@ -13,67 +19,4 @@ public class PatientResponse {
     private LocalTime endTime;
     private AppointmentStatus status;
     private String notes;
-
-
-    public PatientResponse(){
-
-    }
-
-    public PatientResponse(
-            Long appointmentId,
-            String doctorName,
-            String specialization,
-            String departmentName,
-            LocalDate date,
-            LocalTime startTime,
-            LocalTime endTime,
-            AppointmentStatus status,
-            String notes
-    ){
-        this.appointmentId = appointmentId;
-        this.doctorName = doctorName;
-        this.specialization = specialization;
-        this.DepartmentName = departmentName;
-        this.date = date;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.status = status;
-        this.notes = notes;
-    }
-
-    public Long getAppointmentId() {
-        return appointmentId;
-    }
-
-    public String getDoctorName() {
-        return doctorName;
-    }
-
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public String getDepartmentName() {
-        return DepartmentName;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public AppointmentStatus getStatus() {
-        return status;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
 }
