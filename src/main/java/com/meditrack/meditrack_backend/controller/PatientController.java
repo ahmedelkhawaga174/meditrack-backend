c0a63fe29848466cceeb1f8d4dcfaf2fce9913f5
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-09-08 15:19:57 +0300

    add crossorigin on patient controller

--- src/main/java/com/meditrack/meditrack_backend/controller/PatientController.java
+++ src/main/java/com/meditrack/meditrack_backend/controller/PatientController.java
@@ -10,16 +10,14 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/patients")
 @RequiredArgsConstructor
+@CrossOrigin(origins="http://localhost:4200")
 public class PatientController {
 
     private final PatientService patientService;
