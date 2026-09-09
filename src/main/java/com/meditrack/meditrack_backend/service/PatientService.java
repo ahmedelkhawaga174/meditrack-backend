de872efd09ee356b672044b5aaed4d539d50b6cd
Mostafa Kharbita <mostafakharbita@gmail.com>
2026-09-09 07:48:22 +0300

    feat: add patient appointment retrieval

--- src/main/java/com/meditrack/meditrack_backend/service/PatientService.java
+++ src/main/java/com/meditrack/meditrack_backend/service/PatientService.java
@@ -1,6 +1,5 @@
 package com.meditrack.meditrack_backend.service;
 
-
 import com.meditrack.meditrack_backend.entity.Appointment;
 import com.meditrack.meditrack_backend.exception.ResourceNotFoundException;
 import com.meditrack.meditrack_backend.repository.AppointmentRepository;
@@ -15,29 +14,33 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class PatientService {
-    private  final AppointmentRepository appointmentRepository;
-    private  final PatientRepository patientRepository;
 
-    @org.springframework.transaction.annotation.Transactional(readOnly = true)
-    public List<Appointment> getUpcomingAppointments (Long patiendId){
-        ensurePatientsExists(patiendId);
-        return appointmentRepository.findUpcomingByPatientId(patiendId, LocalDate.now());
-    }
+    private final AppointmentRepository appointmentRepository;
+    private final PatientRepository patientRepository;
 
-    private void ensurePatientsExists(Long patiendId) {
+    @Transactional(readOnly = true)
+    public List<Appointment> getUpcomingAppointments(Long patientId) {
+        ensurePatientExists(patientId);
 
+        return appointmentRepository.findUpcomingByPatientId(
+                patientId,
+                LocalDate.now()
+        );
     }
 
     @Transactional(readOnly = true)
-    public List<Appointment> getPastAppointments(Long patientId) {   // no "static"
+    public List<Appointment> getPastAppointments(Long patientId) {
         ensurePatientExists(patientId);
-        return appointmentRepository.findPastByPatientId(patientId, LocalDate.now());
+
+        return appointmentRepository.findPastByPatientId(
+                patientId,
+                LocalDate.now()
+        );
     }
+
     private void ensurePatientExists(Long patientId) {
         if (!patientRepository.existsById(patientId)) {
             throw new ResourceNotFoundException("Patient not found");
         }
     }
-
 }
-
