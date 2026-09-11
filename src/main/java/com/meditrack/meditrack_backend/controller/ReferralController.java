2184418ae6944c6b8828aab1547e35896750ac90
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-09-11 23:06:42 +0300

    add controller for referral feature

---
+++ src/main/java/com/meditrack/meditrack_backend/controller/ReferralController.java
@@ -0,0 +1,24 @@
+package com.meditrack.meditrack_backend.controller;
+
+import com.meditrack.meditrack_backend.dto.ReferralRequest;
+import com.meditrack.meditrack_backend.dto.ReferralResponse;
+import com.meditrack.meditrack_backend.service.ReferralService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/referrals")
+@RequiredArgsConstructor
+public class ReferralController {
+
+    private final ReferralService referralService;
+
+    @PostMapping
+    public ResponseEntity<ReferralResponse> createReferral(@Valid @RequestBody ReferralRequest request) {
+        ReferralResponse response = referralService.createReferral(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
